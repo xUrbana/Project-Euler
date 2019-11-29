4f4d5f3412d9263076ef76efd115e3fed9361866
Ryan Kelley <ryan.kelley@ttu.edu>
2019-11-29 13:28:01 -0600

    Add series helper module

---
+++ euler/series.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+def sum_of_naturals(n):
+    return (n * (n + 1)) // 2
+
+def sum_of_squares(n):
+    return (n * (n + 1) * (2 * n + 1)) // 6
+
+def sum_of_cubes(n):
+    return (n ** 2 * (n + 1) ** 2) // 4
+
+def prod(it):
+    return reduce(lambda x,y: x * y, it, 1)
