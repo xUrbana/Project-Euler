7b894ec8426bdb1208f85c8a0dd1be6c466101a6
Ryan Kelley <ryan.kelley@ttu.edu>
2020-01-07 14:00:11 -0600

    Solve euler 10 and 22

---
+++ euler22.py
@@ -0,0 +1,15 @@
+
+def euler22():
+    
+    with open('data/p022_names.txt') as fp:
+        names = fp.read().split(',')
+        names = [name.strip('"') for name in names]
+    
+    names.sort()
+
+    def name_score(name):
+        return sum(ord(c) - 64 for c in name)
+
+    return sum((i+1)*name_score(name) for i,name in enumerate(names))
+
+print(euler22())
